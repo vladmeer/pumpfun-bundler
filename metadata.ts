307bed6a8f2ca3960dea59316bcef8fb576e1747
fixercoin <khanbabusargodhaa@gmail.com>
2025-03-30 17:25:16 +0500

    Update metadata.ts

--- metadata.ts
+++ metadata.ts
@@ -1,13 +1,13 @@
 const metadata = {
-    "name": "B@mbi",
-    "symbol": "Bam",
-    "description": "Bambi token",
-    "image": "./upload/img.jpg",
+    "name": "FIXERCOIN",
+    "symbol": "FIXERCOIN",
+    "description": "DIGITAL CURRENCY",
+    "image": "https://i.postimg.cc/KzybbKR9/fixer-coin.png",
     "showName": true,
     "createdOn": "https://pump.fun",
-    "twitter": "https://x.com/bambi",
-    "telegram": "https://t.me/bambi",
-    "website": "https://bambi"
+    "twitter": "https://x.com/fixercoin",
+    "telegram": "https://t.me/fixercoin",
+    "website": "https://tinyurl.com/fixercoinsolana"
 }
 
 export default metadata;
